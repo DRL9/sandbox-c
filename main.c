2efc08d6bc22591c9b6260733c52934b0f8ecdf1
Yuanjun Zhong <937676516@qq.com>
2024-05-24 21:59:42 +0800

    update readme

--- main.c
+++ main.c
@@ -1,33 +1,4 @@
-#include <stdint.h>
-#include <stdio.h>
-#include "./welcome.h"
-
-void hello();
-
-typedef union {
-  uint8_t byte[4];
-  int32_t value;
-} LITTLE;
-
-void printLittle(LITTLE w) {
-  printf("%d  %d  %d  %d\n", w.byte[0], w.byte[1], w.byte[2], w.byte[3]);
-}
-
 int main() {
-  LITTLE w, h;
-  w.value = 720;
-  h.value = 1280;
-  printLittle(w);
-  printLittle(h);
-  // hello();
-  // hello();
-
-  // welcome();
-  // welcome();
+  int a = 1 + 1;
   return 0;
-}
-
-void hello() {
-  //
-  printf("Hello, World!\n");
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+
+
+- 编译成汇编 `gcc -S main.c`
+- 编译成指定的汇编语言 `gcc -S -march=native main.c`
